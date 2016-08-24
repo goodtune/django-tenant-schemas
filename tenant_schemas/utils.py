716c029baa5ac78864f93144a5af51249afe5dd7
Justin Wunderle <regolith@gmail.com>
2016-08-24 17:49:13 +1000

    Add TenantContextFilter - put schema_name and domain_url in log records
    Add a LOGGING configuration similar to the section below.
    
        LOGGING = {
            'filters': {
                'tenant_context': {
                    '()': 'tenant_schemas.utils.TenantContextFilter',
                },
            },
            'formatters': {
                'default': {
                    'format': '%(asctime)s [%(levelname)s] '
                              '[%(schema_name)s / %(domain_url)s] '
                              '%(name)s: %(message)s',
                },
            },
            'handlers': {
                'console': {
                    'class': 'logging.StreamHandler',
                    'formatter': 'default',
                    'filters': ['tenant_context'],
                },
            },
        }
    
    Your logging output will not look something like:
    
        13:29 [DEBUG] [my_schema / my-domain.myapp.dev] django.db.backends: (0.001) SELECT ...

--- tenant_schemas/utils.py
+++ tenant_schemas/utils.py
@@ -1,3 +1,5 @@
+import logging
+
 from contextlib import contextmanager
 
 from django.conf import settings
@@ -118,3 +120,14 @@ def app_labels(apps_list):
         return [app.split('.')[-1] for app in apps_list]
     return [AppConfig.create(app).label for app in apps_list]
 
+
+class TenantContextFilter(logging.Filter):
+    """
+    Add the current ``schema_name`` and ``domain_url`` to log records.
+
+    Thanks to @regolith for the snippet on #248
+    """
+    def filter(self, record):
+        record.schema_name = connection.tenant.schema_name
+        record.domain_url = getattr(connection.tenant, 'domain_url', '')
+        return True
